2e36f2326bb3a9b0ff24f5098f429a96c24d6c4a
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-03 13:25:06 +0400

    обернул главный компенент <Provider store={store}>

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
+import { setupStore } from "./store/store";
 
+const store = setupStore();
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+<Provider store={store}>
+  root.render(
+  <App />
+  );
+</Provider>;
