db024c9941957fa67985403e0627b38ff4ed2020
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-04 22:13:32 +0400

    тут обработал все необходимые состояния

--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -1,18 +1,36 @@
 // асинхронный actionCreator - тут сразу не возвращаю новый экшен ,а возвращаю функция которая аргументом примет dispatch и уже из этой функции буду производить асинхронные действия
 
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { IUser } from "../../models/IUser";
 import { AppDispatch } from "../store";
-import { userSlice } from "./useSlice";
+import { userSlice } from "./userSlice";
 
-export const fetchUsers = () => async (dispatch: AppDispatch) => {
-  try {
-    dispatch(userSlice.actions.usersFetching());
-    const response = await axios.get<IUser[]>(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    dispatch(userSlice.actions.usersFetchingSuccess(response.data));
-  } catch (e) {
-    dispatch(userSlice.actions.usersFetchingError(e.message));
+// тут обработал все необходимые состояния
+// export const fetchUsers = () => async (dispatch: AppDispatch) => {
+//   try {
+//     dispatch(userSlice.actions.usersFetching());
+//     const response = await axios.get<IUser[]>(
+//       "https://jsonplaceholder.typicode.com/users"
+//     );
+//     dispatch(userSlice.actions.usersFetchingSuccess(response.data));
+//   } catch (e) {
+//     dispatch(userSlice.actions.usersFetchingError(e.message));
+//   }
+// };
+
+// rejectWithValue которую получим в thunkAPI- это второй аргумент этого callback, который передал вторым аргументом в createAsyncThunk-туда передаю уже какое-то сообщение с ошибкой
+
+export const fetchUsers = createAsyncThunk(
+  "user/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get<IUser[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue("Не удалось загрузить пользователей");
+    }
   }
-};
+);
