929e8e4f13adeac95712b5b588a3a382e54c57f0
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-05 22:17:02 +0400

    RTK query: тут Регистрирую новый редусер  и  добавляю медлвеар из postAPI

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,25 @@ import { useAppDispatch, useAppSelector } from "./hooks/redux";
 import { fetchUsers } from "./store/reducers/ActionCreators";
 
 const App = () => {
-  const dispatch = useAppDispatch();
-  // вытаскиваю необходимые данные из состояния
-  const { users, isLoading, error } = useAppSelector(
-    (state) => state.userReducer
-  );
+  // const dispatch = useAppDispatch();
+  
+   // вытаскиваю необходимые данные из состояния
 
-  // реализовываю получение пользователей для этого нужно за dispatch нужный action
-  useEffect(() => {
-    dispatch(fetchUsers());
-  }, []);
+  // const { users, isLoading, error } = useAppSelector(
+  //   (state) => state.userReducer
+  // );
+
+   // реализовываю получение пользователей для этого нужно за dispatch нужный action
+
+  // useEffect(() => {
+  //   dispatch(fetchUsers());
+  // }, []);
   // визуальное отображение пользователей с помощью JSON.stringify выведем массив пользователей на экран
   return (
     <div className="App">
-      {isLoading && <h1>Идёт загрузка ...</h1>}
+      {/* {isLoading && <h1>Идёт загрузка ...</h1>}
       {error && <h1>{error}</h1>}
-      {JSON.stringify(users, null, 2)}
+      {JSON.stringify(users, null, 2)} */}
     </div>
   );
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
+import { postAPI } from "../services/PostService";
 import userReducer from './reducers/userSlice'
 
 // сосдаю корневой редусер - обьеденяет все редусеры в один
 const rootReducer = combineReducers({
-userReducer
+userReducer,
+ // RTK query тут Регистрирую новый редусер
+[postAPI.reducerPath]: postAPI.reducer
 })
 // конфигурация store
 export const setupStore = () => {
     return configureStore({
-        reducer:rootReducer
+        reducer:rootReducer,
+//  RTK query тут добавляю медлвеар из postAPI
+middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(postAPI.middleware),
     })
 }
 // типы для взаимодействия с нашим хранилищем 1.тип нашего состояния 2.самого стора 3.тип dispatch нашего хранилища
