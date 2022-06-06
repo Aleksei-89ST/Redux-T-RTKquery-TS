a3b80b32c538f7b8dacef866b5ef139d31da0984
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-06 12:24:35 +0400

    опции который будут определять текущий сервис

--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import { IPost } from "../models/IPost";
 
 // reducerPath уникальный ключь который будет определять текущий сервис
 // опции который будут определять текущий сервис
@@ -8,9 +9,12 @@ export const postAPI = createApi({
     baseUrl: "https://jsonplaceholder.typicode.com/",
   }),
   endpoints: (build) => ({
-    fetchAllPosts: build.query({
-      query: () => ({
+    fetchAllPosts: build.query<IPost[], number>({
+      query: (limit: number = 5) => ({
         url: `/posts`,
+        params: {
+            _limit:limit
+        }
       }),
     }),
   }),
